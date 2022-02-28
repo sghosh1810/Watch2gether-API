b612f4c52bb61d46f3cdb3ce9074b4a13bf0cc53
Shounak Ghosh <sghosh1810@gmail.com>
2022-03-01 00:44:44 +0530

    Enable cors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const cron = require("node-cron");
+const cors = require("cors");
+
 const roomDeleteCron = require("./cron/roomDeleteCron");
-require("./db/mongoose");
 const chatRoomRouter = require("./routers/chatRoomRouter");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+require("./db/mongoose");
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +16,8 @@ const server = http.createServer(app);
 const port = process.env.port || 3000;
 
 app.use(express.json());
+app.disable("x-powered-by");
+app.use(cors({ origin: process.env.CORS || "*" }));
 
 app.use("/room", chatRoomRouter);
 
