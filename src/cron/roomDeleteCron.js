6c41b9c7463bd6d60f5ebb750205e3819333b82e
Santanu Garai <santanugarai2015@gmail.com>
2022-02-28 22:21:57 +0530

    chatroom delete time changed

--- src/cron/roomDeleteCron.js
+++ src/cron/roomDeleteCron.js
@@ -4,8 +4,7 @@ const roomDeleteCron = async () => {
     try {
         const rooms = await chatRoom.deleteMany({
             createdAt: {
-                // $gt: ISODate("2020-01-21"),  new Date(new Date() - 7 * 60 * 60 * 24 * 1000)
-                $lte: new Date(new Date() - 1 * 1 * 1000),
+                $lte: new Date(new Date() - 7 * 60 * 60 * 24 * 1000),
             },
         });
         console.log(rooms);
